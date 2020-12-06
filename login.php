c886b2cfe3796db0d3f953cc3545704da2f1136b
tadas0420 <tadas0420@gmail.com>
2020-12-06 15:54:59 +0200

    Login bare bones

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="grid-container">
+        <div class="grid-nav"></div>
+        <div class="grid-main">
+            <form action="post">
+
+                <p><label>Username</label></p>
+                <p><input type="text" name="username"></p>
+                <p><label>Password</label></p>
+                <p><input type="text" name="password"></p>
+                <p><input type="submit" name="submit"></p>
+                 </form>
+
+            
+        </div>
+        <div class="grid-footer">
+            
+        </div>
+    </div>
+</body>
+</html>
