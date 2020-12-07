9937cbc369da5f828be5afc1a79740f8d58743dc
samysosa1 <73364324+samysosa1@users.noreply.github.com>
2020-12-07 13:31:23 +0100

    Update index.php
    main page/  fonts not yet implemented.

--- index.php
+++ index.php
@@ -1,1 +1,91 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Soobway</title>
+    <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+    <div class="main-div">
+      <main>
+        <div class="main-header">
+          <h1>Soobway</h1>
+          <span class="header-tag">The best place to eat away from home</span>
+        </div>
+        <section class="front-menu">
+          <header>
+            <h3>Our Menu</h3>
+            <span>Best Deals</span>
+          </header>
+          <div class="menu-layout columns">
+            <article class="deal-left">
+              <img class="icon-img" src="img/menu-icon.svg" alt="menu icon">
+              <h4 class="deal-title">BURGER1 / $11</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consectetur mattis quam et volutpat.</p>
+            </article>
+             <article class="deal-right">
+               <img class="icon-img" src="img/menu-icon.svg" alt="menu icon">
+              <h4 class="deal-title">FRIES + BURGER 3 + DRINK / $19</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consectetur mattis quam et volutpat.</p>
+            </article>
+            <article class="deal-left">
+              <img class="icon-img" src="img/menu-icon.svg" alt="menu icon">
+              <h4 class="deal-title">BURGER 3 / $9</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consectetur mattis quam et volutpat.</p>
+            </article>
+           <article class="deal-right">
+             <img class="icon-img" src="img/menu-icon.svg" alt="menu icon">
+              <h4 class="deal-title">BURGER 4 / $9</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consectetur mattis quam et volutpat.</p>
+            </article>
+            <article class="deal-left">
+              <img class="icon-img" src="img/menu-icon.svg" alt="menu icon">
+              <h4 class="deal-title">BURGER 5 / $8</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consectetur mattis quam et volutpat.</p>
+            </article>
+            <article class="deal-right">
+              <img class="icon-img" src="img/menu-icon.svg" alt="menu icon">
+              <h4 class="deal-title">FRIES + BURGER5 + DRINK / $18</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consectetur mattis quam et volutpat.</p>
+            </article>
+          </div>
+        </section>
+        <section class="side-dish">
+          <div class="side-layout">
+            <article class="side-right">
+              <img class="side-image" src="img/side-popular.svg">
+              <h4>POPULAR</h4>
+            </article>
+            <article class="side-right">
+              <img class="side-image" src="img/side-chicken.svg">
+              <h4>CHICKEN</h4>
+            </article>
+            <article class="side-right">
+              <img class="side-image" src="img/side-icecream.svg">
+              <h4>ICE CREAM</h4>
+            </article>
+            <article class="side-right">
+              <img class="side-image" src="img/side-fries.svg">
+              <h4>FRIES</h4>
+            </article>
+            <article class="side-right">
+              <img class="side-image" src="img/side-smoothie.svg">
+              <h4>SMOOTHIES</h4>
+            </article>
+            <article class="side-right">
+              <img class="side-image" src="img/side-drink.svg">
+              <h4>DRINKS & MORE</h4>
+            </article>
+          </div>
+        </section>
+        <section class="customer-reminder">
+          <p>Don't forget to tell us about your experience with our food by rating your experience
+            and leaving a comment.</p>
+          <p>See you next time</p>
+        </section>
+      </main>
+      <footer>
+      </footer>
+    </div>
+  </body>
+</html>
 
