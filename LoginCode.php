2c5839b4300ea1c24e8019b482743aa183001de8
HarmenDrenth <72556281+HarmenDrenth@users.noreply.github.com>
2020-12-18 00:59:01 +0100

    Update LoginCode.php
    Log in updated. Fully works

--- LoginCode.php
+++ LoginCode.php
@@ -17,117 +17,89 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-// Initialize the session
-session_start();
- 
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
-    exit;
-}
- 
-// Include config file
-            $conn = mysqli_connect("localhost", "root", "")
-            or die("Could not connect to database!");
- 
-// Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Check if username is empty
-    if(empty(trim($_POST["email"]))){
-        $username_err = "Please enter username.";
-    } else{
-        $username = trim($_POST["email"]);
-    }
-    
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter your password.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT id, email, password FROM users WHERE username = ?";
+        // Initialize the session
+        session_start();
         
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            // Set parameters
-            $param_username = $username;
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-                            session_start();
-                            
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;                            
-                            
-                            // Redirect user to welcome page
-                            header("location: welcome.php");
-                        } else{
-                            // Display an error message if password is not valid
-                            $password_err = "The password you entered was not valid.";
-                        }
-                    }
-                } else{
-                    // Display an error message if username doesn't exist
-                    $username_err = "No account found with that username.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-         $stmt = mysqli_prepare($conn, $sql)
-                    or die("Preperation error");
-                mysqli_stmt_execute($stmt)
-                    or die(mysqli_error($conn));
-                mysqli_stmt_close($stmt);
+        // Check if the user is already logged in, if yes then redirect him to welcome page
+        if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+            header("location: index.php");
+            exit;
         }
+        
+        // Connect to database
+        $conn = mysqli_connect("localhost", "root", "")
+            or die("Could not connect to database!");
+        mysqli_select_db($conn, "Restaurant")
+            or die("Database could not be selected");
+        
+        // Define variables and initialize with empty values
+        $email = $password = "";
+        $email_err = $password_err = "";
+        
+        // Processing form data when form is submitted
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+            // Check if username is empty
+            if(empty(trim($_POST["email"]))){
+                $email_err = "Please enter username.";
+            } else {
+                $email = trim($_POST["email"]);
+            }
+            
+            // Check if password is empty
+            if(empty(trim($_POST["password"]))){
+                $password_err = "Please enter your password.";
+            } else {
+                $password = trim($_POST["password"]);
+            }
+            
+            // Validate credentials
+            if(empty($email_err) && empty($password_err)){
+                // Prepare a select statement
+                $sql = "SELECT id, email, password FROM Customer WHERE email = \"" . $email . "\"";
+                $result = $conn->query($sql);
+        
+                $customer = $result->fetch_assoc();
+                
+                // Check if username exists
+                if($customer != NULL){     
+                    //Verify password
+                    if($customer['password'] == $password){
+                        // Password is correct, so start a new session
+                        session_start();
+                            
+                        // Store data in session variables
+                        $_SESSION["loggedin"] = true;
+                        $_SESSION["id"] = $customer['id'];
+                        $_SESSION["email"] = $email;                            
+                            
+                        // Redirect user to welcome page
+                        header("location: index.php");
+                    }else{
+                        // Display an error message if password is not valid
+                        $password_err = "The password you entered was not valid.";
+                    }
+                }else{
+                    $email_err = "The email you entered has not been registered.";
+                }
+            }
             mysqli_close($conn);
-    }
-    
-    // Close connection
-    mysqli_close($link);
-}
+        }
         ?>
-
-    <div class="wrapper">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                <label>Username</label>
-                <br>
-                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                <br>
+        <div class="wrapper">
+            <h2>Login</h2>
+            <p>Please fill in your credentials to login.</p>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <label>E-mail</label><br>
+                <input type="text" name="email" class="form-control" autocomplete="off" value="<?php echo $email; ?>">
+                <p><?php echo $email_err ?></p>
                 <label>Password</label>
                 <br>
                 <input type="password" name="password" class="form-control">
-                <br>
+                <p><?php echo $password_err ?></p>
                 <input type="submit" class="btn btn-primary" value="Login">
-            <p>Don't have an account? <a href="Registration.php">Sign up now</a>.</p>
-        </form>
-    </div>    
-
+                <p>Don't have an account? <a href="Registration.php">Sign up now</a>.</p>
+            </form>
+        </div>
     </body>
 </html>
