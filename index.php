0c14f61554a74dba497f281d292bf84ed06c3e58
Aggrey <72258112+Aggrey8@users.noreply.github.com>
2020-12-18 21:35:12 +0100

    mennu and style

--- index.php
+++ index.php
@@ -2,27 +2,25 @@
 <html>
    <head>
       <title>Soobway</title>
-     <link rel="stylesheet" type="text/css" href="main.css">
+      <link rel="stylesheet" type="text/css" href="style.css">
    </head>
    <body>
       <div class="grid-container">
          <div class="header">
-           <img class="logo-image" src="img/ourlogo.png">
-               
-                  <nav>
-                     <ul class="nav">
-                        <li><a href="sandwich.html">Home</a> </li>
-                        <li><a href="">Reservation</a> </li>
-                        <li><a href="">Menu</a> </li>
-                        <li><a href="">Location</a> </li>
-                        <li><a href="">Newsupdate</a> </li>
-                        <li><a href="">Sign Up</a> </li>
-                     </ul>
-                  </nav>
-               </div>
-            </div>
-    </header>
-      
+            <img class="logo-image" src="img/ourlogo.png">
+            <nav>
+               <ul class="nav">
+                  <li><a href="sandwich.html">Home</a> </li>
+                  <li><a href="">Reservation</a> </li>
+                  <li><a href="">Menu</a> </li>
+                  <li><a href="">Location</a> </li>
+                  <li><a href="">Newsupdate</a> </li>
+                  <li><a href="">Sign Up</a> </li>
+               </ul>
+            </nav>
+         </div>
+      </div>
+      </header>
       </div>
       <div class="main-div">
          <header class="main-header">
@@ -37,7 +35,6 @@
                </header>
                <div class="menu-layout columns">
                   <article class="deal-left">
-
                      <img class="icon-img" src="img/menu-icon.svg" alt="menu icon">
                      <h4 class="deal-title">BURGER1 / $11</h4>
                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consectetur mattis quam et volutpat.</p>
---
+++ location.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Soobway</title>
+     <link rel="stylesheet" type="text/css" href="main.css">
+   </head>
+   <body>
+      <div class="grid-container">
+         <div class="header">
+           <img class="logo-image" src="img/ourlogo.png">
+           
+               
+                  <nav>
+                     <ul class="nav">
+                        <li><a href="sandwich.html">Home</a> </li>
+                        <li><a href="">Reservation</a> </li>
+                        <li><a href="">Menu</a> </li>
+                        <li><a href="">Location</a> </li>
+                        <li><a href="">Newsupdate</a> </li>
+                        <li><a href="">Sign Up</a> </li>
+                     </ul>
+                  </nav>
+               </div>
+            </div>
+    </header>
+      
+      </div>
