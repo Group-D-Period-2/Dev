0af7048d0854a5621222434fe7bc50fd8c7fb476
seanfclement <70959872+seanfclement@users.noreply.github.com>
2021-01-12 22:14:20 +0100

    Add files via upload

---
+++ About.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UFT-8" content="text/html" content="width=device-width, initial-scale=1" http="content-type">
+	<link rel="stylesheet" href="about.css">
+	<title>SOOBWAY</title>
+</head>
+	<body>
+	
+		    <header>
+				<nav>
+					<img class="logo-image" src="img/ourlogo.png">
+					<ul class="nav">
+					<li><a href="sandwich.html">Home</a> </li>
+					<li><a href="">Reservation</a> </li>
+					<li><a href="">Menu</a> </li>
+					<li><a href="">Location</a> </li>
+					<li><a href="about.html">About Us</a> </li>
+					<li><a href="">Sign Up</a> </li>
+					</ul>
+				</nav>
+            </header>
+
+		<div class="section">
+		<h1>About Us</h1>
+			
+			<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+		</div>
+		
+		<h2>Dine With Us</h2>
+		
+		<div class="Row">
+			<div class="column">
+				<div class="part">
+					<img src="breado.jpg" alt="breado" id="bread">
+					<div class="container">
+					<h2>What We Offer!</h2>
+					<p>We are known for our wide variety of sandwiches passed on from generation to generation. Check out our menu and what we have to offer and be part of the Family!</p>
+					
+					<a href=""
+					<p><button class="button">Menu</button></p></a>
+					</div>
+				</div>	
+			</div>
+
+			<div class="column">
+				<div class="part">
+					<img src="location.png" alt="location" id="link">
+					<div class="container">
+					<h2>Locations Near You</h2>
+					<p>Discover A location near you and visit one of our kitchens and experience the best sandwiches.</p>
+					<a href="About.html">
+					<p><button class="button">Location</button></p></a>
+					</div>
+				</div>	
+			</div>			
+		</div>
+	</body>
+</html>
