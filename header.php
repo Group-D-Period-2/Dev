85702c3d695222f963515d3112ed096e9744efe6
seanfclement <70959872+seanfclement@users.noreply.github.com>
2021-01-12 19:15:07 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UFT-8" content="text/html" http-equiv="content-type">
+	<link rel="stylesheet" href="header.css">
+	<title>Soobway Sandwiches</title>
+</head>
+	<body>
+            <header>
+             <nav>
+			 <img class="logo-image" src="img/ourlogo.png">
+               <ul class="nav">
+                  <li><a href="sandwich.html">Home</a> </li>
+                  <li><a href="">Reservation</a> </li>
+                  <li><a href="">Menu</a> </li>
+                  <li><a href="">Location</a> </li>
+                  <li><a href="about.html">About Us</a> </li>
+                  <li><a href="">Sign Up</a> </li>
+               </ul>
+            </nav>
+            </header>
+         </div>
+		</div>
+
+</body>
+</html>
