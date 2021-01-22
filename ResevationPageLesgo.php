b47cc5b4acb655bcf476106ce72bbb3556c0733a
Beaneaters <71257577+Beaneaters@users.noreply.github.com>
2021-01-22 14:37:09 +0100

    Update ResevationPageLesgo.php

--- ResevationPageLesgo.php
+++ ResevationPageLesgo.php
@@ -79,32 +79,7 @@ include('inc/header.php');
             mysqli_close($conn);
         }
         ?>
-<head>
-	<meta charset="UFT-8" content="text/html" http-equiv="content-type">
-	<link rel="stylesheet" href="header.css">
-	<title>Soobway Sandwiches</title>
-</head>
-	<body>
-            <header>
-             <nav>
-			 <img class="logo-image" src="img/ourlogo.png">
-               <ul class="nav">
-                  <li><a href="sandwich.html">Home</a> </li>
-                  <li><a href="">Reservation</a> </li>
-                  <li><a href="">Menu</a> </li>
-                  <li><a href="">Location</a> </li>
-                  <li><a href="about.html">About Us</a> </li>
-                  <li><a href="">Sign Up</a> </li>
-                  <li><a href="">Login</a> </li>
-               </ul>
-            </nav>
-            </header>
-	</body>
-</html>
 
-
-
-        
     <div class="form-res">
         <form class="res-form shadow" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
         <h1 id="res-title">Reservation</h1>
